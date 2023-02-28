906b81938d064748087a39b2b98049857e42e300
Artem09222 <107929764+Artem09222@users.noreply.github.com>
2023-02-28 16:22:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// Ex 1
+
+let a = 3; 
+
+while (a < 30) {
+    a++;
+    console.log(a);
+}
+
+// Ex 2 
+
+let b = 5;
+
+while (b > -10) {
+    b--;
+    console.log(b);
+}
+
+//Ex 3
+
+let c = 0;
+let d;
+
+while (c != d) {
+    d = prompt("Enter number")
+}
